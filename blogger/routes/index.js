98074176937f45970b70ee5dc5991faa47953910
Debian <bitnami@ip-172-26-3-86.ec2.internal>
2020-09-21 23:04:33 +0000

    lab2 406

---
+++ blogger/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var ctrlHome = require('../controllers/home');
+var ctrlBlog = require('../controllers/blog');
+
+/* Setup routes to pages. */
+router.get('/', ctrlHome.home);
+//router.get('/blog', ctrlBlog.blog);
+router.get('/bloglist', ctrlBlog.bloglist);
+router.get('/blogadd', ctrlBlog.blogadd);
+
+module.exports = router;
