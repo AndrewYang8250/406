2860548b045557b01b65842beacba22fe2c44b16
AndrewYang8250 <70337629+AndrewYang8250@users.noreply.github.com>
2020-10-14 22:37:04 -0400

    Delete blog.js

--- blogger/controllers/blog.js
+++
@@ -1,9 +0,0 @@
-/*GET blog list page. */
-module.exports.bloglist = function(req, res) {
-	res.render('bloglist', { title: 'Blog List' });
-};
-
-/*GET blog add page. */
-module.exports.blogadd = function(req, res) {
-	res.render('blogadd', { title: 'Blog Add' });
-};
