122b2cc9fdaf4d534a36961e6827f27605e20346
AndrewYang8250 <70337629+AndrewYang8250@users.noreply.github.com>
2020-10-14 22:43:14 -0400

    Update home.js

--- blogger/app_server/controllers/home.js
+++ blogger/app_server/controllers/home.js
@@ -1,4 +1,4 @@
-/*GET home page. */
+
 module.exports.home = function(req, res) {
-	res.render('home', { title: 'Andrew Yangs Blog Site' });
+	res.render('index', { title: 'Andrew Yangs Blog Site' });
 };
