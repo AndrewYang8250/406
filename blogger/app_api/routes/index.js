a01d7d0dd03591db4b1a8551ae7559cdd1f326b3
AndrewYang8250 <70337629+AndrewYang8250@users.noreply.github.com>
2020-10-14 22:40:04 -0400

    Update index.js

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var router = express.Router();
+
 var ctrlBlog = require('../controllers/blog');
 
-/* Setup blog functions. */
-/* get list of blogs */
 router.get('/blogs', ctrlBlog.blogList);
-/* get one blog */
-router.get('/blogs/:blogid', ctrlBlog.blogReadOne);
-/* add blog */
 router.post('/blogs', ctrlBlog.blogCreate);
-/* update blog */
+router.get('/blogs/:blogid', ctrlBlog.blogReadOne);
 router.put('/blogs/:blogid', ctrlBlog.blogUpdateOne);
-/* delete blog */
 router.delete('/blogs/:blogid', ctrlBlog.blogDeleteOne);
 
 module.exports = router;
