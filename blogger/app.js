aabf2383a3ad57f531d609957c78cca94f6cbe98
AndrewYang8250 <70337629+AndrewYang8250@users.noreply.github.com>
2020-10-26 22:13:29 -0400

    Update app.js

--- blogger/app.js
+++ blogger/app.js
@@ -3,43 +3,49 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
+require('./app_api/models/db');
+
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/public/stylesheets'));
+app.use('/webfonts', express.static(__dirname + '/public/fonts/webfonts/'));
+
+
+app.use('/js', express.static(__dirname + '/node_modules/angular'));
+app.use('/js', express.static(__dirname + '/node_modules/angular-route'));
+app.use('/js', express.static(__dirname + '/node_modules/angular-ui-router/release'));
+app.use('/js', express.static(__dirname + '/app_client'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.use('/css', express.static(__dirname + '/public/stylesheets'));
+app.use(express.static(path.join(__dirname, 'app_client')));
+
+app.use('/api', routesApi);
 
 
-app.use('/', indexRouter);
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
